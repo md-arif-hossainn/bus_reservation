ce0ebb8ee314cda2af380e7a6a3fef1018d09b3f
hossain_arif <arif134221@gmail.com>
2025-01-29 10:45:38 +0600

    Dummy data source and reservation file added

---
+++ lib/models/reservation_expansion_item.dart
@@ -0,0 +1,45 @@
+
+import 'bus_schedule.dart';
+import 'customer.dart';
+
+class ReservationExpansionItem {
+  ReservationExpansionHeader header;
+  ReservationExpansionBody body;
+  bool isExpanded;
+
+  ReservationExpansionItem({
+    required this.header,
+    required this.body,
+    this.isExpanded = false,
+  });
+}
+
+class ReservationExpansionHeader {
+  int? reservationId;
+  String departureDate;
+  BusSchedule schedule;
+  int timestamp;
+  String reservationStatus;
+
+  ReservationExpansionHeader({
+    required this.reservationId,
+    required this.departureDate,
+    required this.schedule,
+    required this.timestamp,
+    required this.reservationStatus,
+  });
+}
+
+class ReservationExpansionBody {
+  Customer customer;
+  int totalSeatedBooked;
+  String seatNumbers;
+  int totalPrice;
+
+  ReservationExpansionBody({
+    required this.customer,
+    required this.totalSeatedBooked,
+    required this.seatNumbers,
+    required this.totalPrice,
+  });
+}
